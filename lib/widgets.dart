333ea0aafa6f23b70649a198db124fc3d3608a92
Waldemar <vladdigen@icloud.com>
2024-08-25 20:20:24 +0400

    Building the timer home page layout

---
+++ lib/widgets.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProductivityButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final double size;
+  final VoidCallback onPressed;
+
+  const ProductivityButton({super.key, 
+      required this.color,
+      required this.text,
+      required this.onPressed,
+      this.size = 0,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: color,
+      minWidth: size,
+      child: Text(
+        text,
+        style: const TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
